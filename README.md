e628cac6ca6429d8d12f2b850311279720abc626
Belén <alonsoperalbelen@gmail.com>
2026-06-09 11:08:04 +0200

    added cookie consent project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-https://roadmap.sh/projects/simple-tabs
+https://roadmap.sh/projects/cookie-consent
 
 *Tabs
 
--- iteration2.js
+++ iteration2.js
@@ -1,11 +1,29 @@
 function showTab(tabId) {
-    if (tabId === "firstTab") {
-      document.getElementById("showTab").innerHTML = "First Tab content.";
-    } else if (tabId === "secondTab") {
-      document.getElementById("showTab").innerHTML = "Second Tab content.";
-    } else if (tabId === "thirdTab") {
-        document.getElementById("showTab").innerHTML = "Third Tab content.";
-    } else if (tabId === "fourthTab") {
-        document.getElementById("showTab").innerHTML = "Fourth Tab content.";
-    }
+  if (tabId === "firstTab") {
+    document.getElementById("showTab").innerHTML = "First Tab content.";
+  } else if (tabId === "secondTab") {
+    document.getElementById("showTab").innerHTML = "Second Tab content.";
+  } else if (tabId === "thirdTab") {
+    document.getElementById("showTab").innerHTML = "Third Tab content.";
+  } else if (tabId === "fourthTab") {
+    document.getElementById("showTab").innerHTML = "Fourth Tab content.";
+  }
 }
+
+function closeTag() {
+  document.getElementById("cookieConsent").style.display = "none";
+}
+
+function acceptCookies() {
+  localStorage.setItem("cookiesAccepted", "true");
+  closeTag();
+}
+
+function checkCookies() {
+  const cookiesAccepted = localStorage.getItem("cookiesAccepted");
+  if (cookiesAccepted === "true") {
+    closeTag();
+  }
+}
+
+addEventListener("DOMContentLoaded", checkCookies); // "Cuando el HTML esté listo, ejecuta checkCookies automáticamente"
